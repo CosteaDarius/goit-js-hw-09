584a93cbcaa96d16f7e00f2376fb4686c60b29a6
CosteaDarius <dariuscostea068@gmail.com>
2024-12-05 22:19:55 +0200

    c

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function startColorSwitcher() {
+  if (intervalId !== null) return;
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorSwitcher() {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+}
+
+startButton.addEventListener('click', startColorSwitcher);
+stopButton.addEventListener('click', stopColorSwitcher);
